72e0f28baa16b55e9c50d7d3d2d523a561587db8
Carmen <czunigam@uoc.edu>
2025-12-17 00:37:11 +0100

    edit aws

--- producto3/database/seeders/DatabaseSeeder.php
+++ producto3/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // ADMIN
+User::create([
+    'name' => 'Admin',
+    'email' => 'admin@example.com',
+    'password' => Hash::make('admin123'),
+    'role' => 'admin',
+]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+// HOTEL 1
+User::create([
+    'name' => 'Hotel Demo',
+    'email' => 'hotel@example.com',
+    'password' => Hash::make('hotel123'),
+    'role' => 'hotel',
+    'hotel_id' => 1,
+]);
+
+// HOTEL 2 (si quieres un usuario normal, lo tratamos como hotel sin hotel_id)
+User::create([
+    'name' => 'Usuario',
+    'email' => 'user@example.com',
+    'password' => Hash::make('user123'),
+    'role' => 'hotel',
+    'hotel_id' => null,
+]);
     }
 }
